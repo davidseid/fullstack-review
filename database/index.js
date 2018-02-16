60fa8abdc45d29172d7984cdfcefcd87139f33de
David Seidenberg <davidseidenberg@gmail.com>
2018-02-16 14:09:41 -0800

    Wrote schema and document saving function

--- database/index.js
+++ database/index.js
@@ -1,16 +1,28 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection: error:'));
+db.once('open', function() {
+  console.log('connected!');
+})
+
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  id: Number,
+  repo: String
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = (id, repoAsJSON) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+  var newRepo = new Repo({
+    id: id,
+    repo: repoAsJSON
+  });
 }
 
 module.exports.save = save;
