cbfab4e24bf4963b7a5a85083cd8db38b4fd75be
David Seidenberg <davidseidenberg@gmail.com>
2018-02-16 22:07:24 -0800

    Rendering 25 repos to the DOM with a tiny bit of styling

--- client/src/components/Repo.jsx
+++ client/src/components/Repo.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 const Repo = (props) => {
   return (
     <div className="repo">
-      Repo
+      <div>
+        {props.repoData.id}
+      </div>
+      <div>
+        {props.repoData.repo_name}
+      </div>
+      <div>
+        {props.repoData.url}
+      </div>
     </div>
   )
 }
